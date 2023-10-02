d611967020250335d26d4ff68ef70f20b32340aa
Al Wirtes <wirtes@gmail.com>
2023-10-02 07:26:02 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,5 +18,5 @@ my_fruit_list = my_fruit_list.set_index("Fruit")
 streamlit.dataframe(my_fruit_list)
 
 ## Lesson 3: Add a Streamlit Multiselect Picker
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
