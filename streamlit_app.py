999ab89b4554ce06839e0468025da0fe79b71034
Al Wirtes <wirtes@gmail.com>
2023-10-02 07:06:39 -0600

    Now with pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -7,3 +9,9 @@ streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+## Lesson 3: Importing Pandas
+## Read the CSV from the S3 bucket
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+## Display the CSV in Streamlit
+streamlit.dataframe(my_fruit_list)
