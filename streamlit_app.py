efef5f1e214e38c852dc8660b4d668375c6d6e1e
Al Wirtes <wirtes@gmail.com>
2023-10-03 12:55:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,5 +25,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table
 streamlit.dataframe(fruits_to_show)
 
+# New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruice Advice!')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
