3a5f0835e6e823e0df3f35b1b9a6f4719af7124a
Al Wirtes <wirtes@gmail.com>
2023-10-03 13:59:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,8 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header('Fruityvice Fruice Advice!')
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 # GET from the fruityvice API
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
