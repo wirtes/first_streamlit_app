0afa180f8ddaddc0e8261cc8c17dc8a82ca14d67
Al Wirtes <wirtes@gmail.com>
2023-10-04 11:13:17 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,19 +28,18 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
-streamlit.header('Fruityvice Fruice Advice!')
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+streamlit.header('Fruityvice Fruit Advice!')
 
-# GET from the fruityvice API
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Normalize JSON response into a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Write it to the page
-streamlit.dataframe(fruityvice_normalized)
-
-streamlit.stop()
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # Snowflake Connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
@@ -56,3 +55,14 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 
 # Where we purposely trash our database table to learn a valuable lesson
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
+
+
+
+
+
+
+
+
+
+
